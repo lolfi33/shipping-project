ffbc266e5d46f15c6b3f4d644854c11698cba85e
lolfi33 <yoayoann@gmail.com>
2025-02-25 12:47:04 +0100

    fix port 2

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
-  await app.listen(3000);
+
+  const port = process.env.PORT || 3000; // Utiliser le port fourni par Render
+  await app.listen(port);
+  console.log(`🚀 Server running on port ${port}`);
 }
 bootstrap();
